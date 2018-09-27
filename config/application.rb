74bb5125c77b49d84d6caebfb16757b19d7c7564
Wanderson <wandersonelias55@gmail.com>
2018-09-27 15:22:02 -0300

    Inicial - 4

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Barganhaki
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
     # Initialize configuration defaults for originally generated Rails version.
     #config.load_defaults 5.2
     
