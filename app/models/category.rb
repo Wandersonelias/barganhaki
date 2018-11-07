2813013b60ce2a4534870749df87d826104bc5b0
Wanderson <wandersonelias55@gmail.com>
2018-11-07 00:44:30 -0300

    Integração com o aws final - Correção do icones de categoias para a AWS

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,12 +1,28 @@
 class Category < ApplicationRecord
     validates_presence_of :description
 
-    has_attached_file :icon, styles: { medium: "300x300>", thumb: "50x50>" }, default_url: "/images/:style/missing.png"
-    validates_attachment_content_type :icon, content_type: /\Aimage\/.*\z/
+    #has_attached_file :icon, styles: { medium: "300x300>", thumb: "50x50>" }, default_url: "/images/:style/missing.png"
+    #validates_attachment_content_type :icon, content_type: /\Aimage\/.*\z/
 
 
 #tornando url amigaveis para SEO
 
- 
+  def icon=(value) #overload pesquisar
+    if value.is_a?(String)
+      super(value)
+       
+    else
+      img = AwsService.upload(value.tempfile.path, value.original_filename, "50x50")
+      super(img)
+      
+    end
+  end
+
+  
+  
+  def icon
+    return super if super.present?
+    return "/img/aviao.png"
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_07_022350) do
+ActiveRecord::Schema.define(version: 2018_11_07_034253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2018_11_07_022350) do
     t.string "icon_content_type"
     t.bigint "icon_file_size"
     t.datetime "icon_updated_at"
+    t.string "icon"
   end
 
   create_table "compras", force: :cascade do |t|
