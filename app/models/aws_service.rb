48bacf41748c040577575d0434ae4b46962d41c8
Wanderson <wandersonelias55@gmail.com>
2018-11-14 21:59:18 -0300

    remoçao de debbuger do codigo

--- app/models/aws_service.rb
+++ app/models/aws_service.rb
@@ -26,7 +26,7 @@ class AwsService
             image.crop "#{crop}+0+0"
             image.write file
         end
-        debugger
+    
         obj = s3.bucket(BUCKET).object("#{Time.now.to_i}-#{name}")
         obj.upload_file(file, acl:'public-read')
         obj.public_url
@@ -40,9 +40,4 @@ class AwsService
     end
 end 
 
-#AWS_ACCESS_KEY_ID="AKIAJBIHKW46GWAOBOCQ" AWS_SECRET_ACCESS_KEY="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" S3_BUCKET_NAME ="barganha-images" rails s
 
-#heroku config:set S3_BUCKET_NAME="barganha-images"
-#heroku config:set AWS_ACCESS_KEY_ID="AKIAJBIHKW46GWAOBOCQ"
-#heroku config:set AWS_SECRET_ACCESS_KEY="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"
-#heroku config:set AWS_REGION="us-east-1"
