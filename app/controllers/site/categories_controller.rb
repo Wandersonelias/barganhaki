27d4eee82c341b0c71f55af2404b1f06c5bc407f
Wanderson <wandersonelias55@gmail.com>
2018-11-21 15:24:16 -0300

    detecção de categorias e pagina de erros

--- app/controllers/site/categories_controller.rb
+++ app/controllers/site/categories_controller.rb
@@ -1,13 +1,19 @@
 class Site::CategoriesController < ApplicationController
     layout "site"
     def show
-        @categories = Category.all
-        @categoria = Category.find(params[:id])
-        @products = Product.where(:category_id => @categoria.id)
         if @carrinho = cookies[:carrinho].present?
             @carrinho = JSON.parse(cookies[:carrinho])
           else
             @carrinho = []
         end
+        @categories = Category.all
+        @categoria = Category.find(params[:id])
+        @products = Product.where(:category_id => @categoria.id)
+        if @products.empty?
+            render :errorcat
+        else
+            render :show
+        end
+       
     end
 end
