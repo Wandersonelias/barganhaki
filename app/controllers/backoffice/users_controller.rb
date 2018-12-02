be6034eeac8cdd0210e6914af3f2b188f31ced87
Wanderson <wandersonelias55@gmail.com>
2018-12-02 20:16:52 -0300

    ajustes nos hashs situação dos produtos profile edit e new

--- app/controllers/backoffice/users_controller.rb
+++ app/controllers/backoffice/users_controller.rb
@@ -1,6 +1,39 @@
 class Backoffice::UsersController < ApplicationController
   layout "backoffice"
+  
+  before_action :set_users, only: [:edit, :update]
+
   def index
     @users = User.salers_actives
   end
+  def edit
+
+  end
+  def update
+    passwd = params[:user][:password]
+    passwd_confirm = params[:user][:password_confirmation]
+    #faz a verificação se o password esta em branco
+    # e ativa o metodo delete para contornar o 
+    if passwd.blank? && passwd_confirm.blank?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+    if @admin = User.update(params_users)
+      redirect_to backoffice_users_path , notice: "Mensagem"
+    else
+      render :edit   , notice: "Mensagem"
+    end
+
+  end
+
+  private
+  def set_users
+    @user = User.find(params[:id])
+  end
+
+  def params_users
+    params.require(:user).permit(:id, :name, :email, :password, :password_confirmation, :status)
+  end
+
+
 end
